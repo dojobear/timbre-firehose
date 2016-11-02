3787029519d53e3548bf1cd10e655f1edc7153c8
dojobear <bear.essentials19@gmail.com>
2016-11-02 17:56:09 -0400

    adding firehose appender logic

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"] 
+                 [cheshire "5.4.0" :exclusions [com.fasterxml.jackson.core/jackson-core]]
+                 [com.taoensso/timbre "4.7.4"]
+                 [amazonica "0.3.77"]])
--- src/timbre_firehose/core.clj
+++ src/timbre_firehose/core.clj
@@ -1,6 +1,38 @@
-(ns timbre-firehose.core)
+(ns timbre-firehose.core
+  (:require [cheshire.core :as json]
+            [taoensso.timbre :as timbre]
+            [amazonica.aws.kinesisfirehose :as fh]
+            [amazonica.core :refer [with-credential]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmacro with-aws-cred
+  "If AWS credentials are supplied, then call the block using Amazonica's
+   `with-credential`, otherwise just execute the block directly."
+  [credential & body]
+  `(if ~credential
+     (with-credential ((juxt :access-key :secret-key :endpoint) ~credential)
+       ~@body)
+     ~@body))
+
+(defn- create-output
+  [data]
+  (let [{:keys [level error-level? vargs_ ?file ?line ?err_ ?ns-str msg_]} data]
+    (json/generate-string (merge {:level level
+                                  :message @vargs_
+                                  :file ?file
+                                  :line ?line
+                                  :ns ?ns-str}
+                                 (when-let [e @?err_]
+                                   {:error {:stacktrace (timbre/stacktrace e {:stacktrace-fonts {}})
+                                            :exception-message (.getMessage e)
+                                            :error-level error-level?}})))))
+
+(defn firehose-appender
+  [{:keys [stream-name credential]}]
+  {:async? false
+   :enabled? true
+   :min-level nil
+   :output-fn (fn [data]
+                (create-output data))
+   :fn (fn [data]
+         (let [{:keys [output-fn]} data]
+           (with-aws-cred credential (fh/put-record stream-name (output-fn data)))))})
