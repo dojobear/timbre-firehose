85b1399713f9c50acd13f3f599167b3ab2103e2a
dojobear <bear.essentials19@gmail.com>
2016-11-03 14:16:29 -0400

    removing whitespace

--- test/timbre_firehose/core_test.clj
+++ test/timbre_firehose/core_test.clj
@@ -7,14 +7,4 @@
 (fact "invoking timbre with a firehose appender makes the correct calls to amazonica."
   (do (timbre/set-config! {:level :debug :appenders {:firehose (target/firehose-appender {:stream-name "test"})}})
       (timbre/error "error test message") => nil
-      (provided (fh/put-record "test" #"\{\"level\":\"error\",\"message\":\[\"error test message\"\],\"file\":\".*/timbre-firehose/test/timbre_firehose/core_test.clj\",\"line\":9,\"ns\":\"timbre-firehose.core-test\"\}") => nil)))
-
-
-
-
-
-
-
-
-
-
+      (provided (fh/put-record "test" #"\{\"level\":\"error\",\"message\":\[\"error test message\"\],\"file\":\".*timbre_firehose/core_test.clj\",\"line\":9,\"ns\":\"timbre-firehose.core-test\"\}") => nil)))
