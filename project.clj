9a3d9f58b31be6d519d1a0b20879f93436fe836c
dojobear <bear.essentials19@gmail.com>
2016-11-03 14:11:38 -0400

    adding tests for appender

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject timbre-firehose "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :plugins [[lein-midje "3.1.3"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"] 
                  [cheshire "5.4.0" :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [com.taoensso/timbre "4.7.4"]
-                 [amazonica "0.3.77"]])
+                 [amazonica "0.3.77"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
--- test/timbre_firehose/core_test.clj
+++ test/timbre_firehose/core_test.clj
@@ -1,7 +1,20 @@
 (ns timbre-firehose.core-test
-  (:require [clojure.test :refer :all]
-            [timbre-firehose.core :refer :all]))
+  (:use midje.sweet)
+  (:require [timbre-firehose.core :as target]
+            [taoensso.timbre :as timbre]
+            [amazonica.aws.kinesisfirehose :as fh]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(fact "invoking timbre with a firehose appender makes the correct calls to amazonica."
+  (do (timbre/set-config! {:level :debug :appenders {:firehose (target/firehose-appender {:stream-name "test"})}})
+      (timbre/error "error test message") => nil
+      (provided (fh/put-record "test" #"\{\"level\":\"error\",\"message\":\[\"error test message\"\],\"file\":\".*/timbre-firehose/test/timbre_firehose/core_test.clj\",\"line\":9,\"ns\":\"timbre-firehose.core-test\"\}") => nil)))
+
+
+
+
+
+
+
+
+
+
